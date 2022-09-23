523ddeacc41d521cad21432e023fd05175596a6c
Geoffroy Couprie <geoffroy@apollographql.com>
2022-09-23 15:26:27 +0200

    parse query and token

--- src/plugins/biscuit.rs
+++ src/plugins/biscuit.rs
@@ -1,22 +1,26 @@
+use apollo_compiler::values::OperationType;
 use apollo_router::layers::ServiceBuilderExt;
 use apollo_router::plugin::Plugin;
 use apollo_router::plugin::PluginInit;
 use apollo_router::register_plugin;
 use apollo_router::services::subgraph;
 use apollo_router::services::supergraph;
+use biscuit::macros::authorizer;
+use biscuit_auth as biscuit;
 use schemars::JsonSchema;
 use serde::Deserialize;
-use std::ops::ControlFlow;
 use tower::BoxError;
 use tower::ServiceBuilder;
 use tower::ServiceExt;
 
-use biscuit_auth as biscuit;
+use std::error::Error;
+use std::ops::ControlFlow;
 
 #[derive(Debug)]
 struct Biscuit {
     #[allow(dead_code)]
     configuration: Conf,
+    root: biscuit::PublicKey,
 }
 
 #[derive(Debug, Default, Deserialize, JsonSchema)]
@@ -25,6 +29,8 @@ struct Conf {
     // Always put some sort of config here, even if it is just a bool to say that the plugin is enabled,
     // otherwise the yaml to enable the plugin will be confusing.
     message: String,
+
+    public_root: String,
 }
 // This plugin is a skeleton for doing authentication that requires a remote call.
 #[async_trait::async_trait]
@@ -33,8 +39,11 @@ impl Plugin for Biscuit {
 
     async fn new(init: PluginInit<Self::Config>) -> Result<Self, BoxError> {
         tracing::info!("{}", init.config.message);
+
+        let root = biscuit::PublicKey::from_bytes_hex(&init.config.public_root)?;
         Ok(Biscuit {
             configuration: init.config,
+            root,
         })
     }
 
@@ -62,8 +71,89 @@ impl Plugin for Biscuit {
     }
 }
 
-fn validate_request(request: &mut supergraph::Request) -> Result<(), String> {
-    todo!()
+impl Biscuit {
+    fn validate_request(&self, request: &mut supergraph::Request) -> Result<(), BoxError> {
+        let compiler = apollo_compiler::ApolloCompiler::new(
+            &request
+                .supergraph_request
+                .body()
+                .query
+                .as_deref()
+                .expect("there should be a query by now"),
+        );
+
+        let ops = compiler.operations();
+
+        let operation = match request.supergraph_request.body().operation_name.as_ref() {
+            None => ops.get(0),
+            Some(name) => ops.iter().find(|op| op.name() == Some(name)),
+        };
+
+        let operation = match operation {
+            None => {
+                return Err(Box::<dyn Error + Send + Sync>::from(
+                    "cannot find operation",
+                ))
+            }
+            Some(op) => op,
+        };
+
+        let mut authorizer = authorizer!(
+            r#"    
+    // a verifier can come with allow/deny policies. While checks are all tested
+    // and must all succeeed, allow/deny policies are tried one by one in order,
+    // and we stop verification on the first that matches
+    //
+    // here we will check that the token has the corresponding right
+    allow if right("/a/file1.txt", "read");
+    // explicit catch-all deny. here it is not necessary: if no policy
+    // matches, a default deny applies
+    deny if true;
+ "#,
+        );
+
+        let operation_type = operation.operation_ty();
+        for root_op in operation.fields(&compiler.db).iter() {
+            match operation_type {
+                OperationType::Query => {
+                    authorizer.add_fact(format!("query(\"{}\")", root_op.name()).as_str())?
+                }
+                OperationType::Mutation => {
+                    authorizer.add_fact(format!("mutation(\"{}\")", root_op.name()).as_str())?
+                }
+                OperationType::Subscription => {
+                    authorizer.add_fact(format!("subscription(\"{}\")", root_op.name()).as_str())?
+                }
+            }
+        }
+
+        let opt_token_str = match request.supergraph_request.headers().get("Authorization") {
+            None => None,
+            Some(value) => {
+                let value = value.to_str()?;
+                if !value.starts_with("Bearer ") {
+                    return Err(Box::<dyn Error + Send + Sync>::from("not a bearer token"));
+                }
+                Some(&value[6..])
+            }
+        };
+        let opt_token = match opt_token_str {
+            None => None,
+            Some(s) => Some(biscuit::Biscuit::from_base64(s, &self.root)?),
+        };
+
+        if let Some(token) = opt_token.as_ref() {
+            authorizer.add_token(token)?;
+        }
+
+        authorizer.authorize()?;
+
+        if let Some(token) = opt_token_str {
+            request.context.insert("biscuit", token.to_string())?;
+        }
+
+        Ok(())
+    }
 }
 
 // This macro allows us to use it in our plugin registry!
